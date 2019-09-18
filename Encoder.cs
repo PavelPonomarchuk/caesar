bf9262e7c58e65feba90311abf0e42ac00e8423c
PavelPonomarchuk <37922060+PavelPonomarchuk@users.noreply.github.com>
2019-09-18 22:53:52 +0500

    Add files via upload

---
+++ Encoder.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caesar2
+{
+    public class Encoder
+    {
+        private char[] alphabet = new char[]
+            { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п',
+                'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я' };
+
+        public string Clean(string openMessage)
+        {
+            var builder = new StringBuilder();
+            
+            foreach (var symbol in openMessage)
+            {
+                var sym = symbol;
+                if (char.IsUpper(sym)) sym = char.ToLower(sym);
+                foreach (var c in alphabet)
+                    if (sym == c) builder.Append(sym);
+            }
+            var cleanMessage = builder.ToString();
+            return cleanMessage;
+        }
+
+        public string Encode(string openMessage)
+        {            
+            int index = 0;
+            var builder = new StringBuilder();
+            string cleanMessage = Clean(openMessage);
+
+            foreach (var letter in cleanMessage)
+            {
+                for (int i = 0; i < 33; i++)
+                {                    
+                    if (letter == alphabet[i])
+                    {
+                        index = i;
+                        break;
+                    }
+                }
+                if (index <= 29) index = index + 3;
+                else if (index > 29) index = index - 30;                                
+                builder.Append(alphabet[index]);
+            }
+            var encodedMessage = builder.ToString();
+            return encodedMessage;
+        }
+
+        public string Decode(string encodedMessage)
+        {
+            int index = 0;
+            var builder = new StringBuilder();
+            
+            foreach (var letter in encodedMessage)
+            {
+                for (int i = 0; i < 33; i++)
+                {
+                    if (letter == alphabet[i])
+                    {
+                        index = i;
+                        break;
+                    }
+                }
+                if (index >= 3) index = index - 3;
+                else if (index < 3) index = index + 30;
+                builder.Append(alphabet[index]);
+            }
+            var decodedMessage = builder.ToString();
+            return decodedMessage;
+        }
+    }
+}
---
+++ MyForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace caesar2
+{
+    class MyForm : Form
+    {
+        
+        public MyForm()
+        {
+            var labelInput = new Label
+            {
+                Location = new Point(0, 0),
+                Size = new Size(ClientSize.Width, 30),
+                Text = "Введите исходный текст:"
+            };
+            var boxInput = new TextBox
+            {
+                Location = new Point(0, labelInput.Bottom),
+                Size = new Size(ClientSize.Width, 100)
+            };
+            var labelOutput = new Label
+            {
+                Location = new Point(0, boxInput.Bottom),
+                Size = labelInput.Size,
+                Text = "Результат:"
+            };
+            var boxOutput = new TextBox
+            {
+                Location = new Point(0, labelOutput.Bottom),
+                Size = boxInput.Size
+            };
+            var buttonEncode = new Button
+            {
+                Location = new Point(0, boxOutput.Bottom),
+                Size = labelOutput.Size,
+                Text = "Зашифровать"
+            };
+            var buttonDecode = new Button
+            {
+                Location = new Point(0, buttonEncode.Bottom),
+                Size = labelOutput.Size,
+                Text = "Расшифровать"
+            };
+            Controls.Add(labelInput);
+            Controls.Add(boxInput);
+            Controls.Add(labelOutput);
+            Controls.Add(boxOutput);
+            Controls.Add(buttonEncode);
+            Controls.Add(buttonDecode);
+
+            buttonEncode.Click += (sender, args) =>
+            {
+                var encoder = new Encoder();
+                boxOutput.Text = encoder.Encode(boxInput.Text);
+            };
+            buttonDecode.Click += (sender, args) =>
+            {
+                var encoder = new Encoder();
+                boxOutput.Text = encoder.Decode(boxInput.Text);
+            };
+        }        
+    }
+}
